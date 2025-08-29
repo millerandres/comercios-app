5c36768549d1776631a2abd26e9a7e9c56977219
Miller <ingenierogom@gmail.com>
2025-08-28 23:35:21 -0500

    se inicia la estructura del proyecto con sus dependencias

---
+++ comercios/src/main/java/com/comercios/comercios/ComerciosApplication.java
@@ -0,0 +1,13 @@
+package com.comercios.comercios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComerciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComerciosApplication.class, args);
+	}
+
+}
---
+++ comercios/src/test/java/com/comercios/comercios/ComerciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comercios.comercios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComerciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
